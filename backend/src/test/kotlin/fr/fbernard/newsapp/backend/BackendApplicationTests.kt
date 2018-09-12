be3cadf6410b50d424e420fafe171ee8c9dd3304
fbernard <fbernard@appstud.com>
2018-09-12 20:45:15 +0200

    init kotlin backend with springboot

---
+++ backend/src/test/kotlin/fr/fbernard/newsapp/backend/BackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package fr.fbernard.newsapp.backend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

98211de76be872209a949f641d70f9d94c1400fd
fbernard <fbernard@appstud.com>
2018-09-27 21:44:31 +0200

    [IOS] init xcode project

---
+++ ios/ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by florian bernard on 27/09/2018.
+//  Copyright © 2018 florian bernard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

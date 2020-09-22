39a53279d0a3baa198dfd1620859a51e31886130
fb64 <florian.bernard64@gmail.com>
2020-09-23 00:26:53 +0200

    [Backend] refactor backend by using kotlin with coroutines

--- backends/kotlin-graal/src/main/kotlin/fr/fbernard/newsapp/backend/NewsService.kt
+++ backends/kotlin-graal/src/main/kotlin/fr/fbernard/newsapp/backend/NewsService.kt
@@ -2,37 +2,32 @@ package fr.fbernard.newsapp.backend
 
 import fr.fbernard.grpc.news.*
 import fr.fbernard.newsapp.backend.data.NewsStore
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.Single
-import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filter
 
-class NewsService : RxNewsServiceGrpc.NewsServiceImplBase() {
+class NewsService : NewsServiceGrpcKt.NewsServiceCoroutineImplBase() {
 
-    val addNewEvent = PublishSubject.create<News>()
+    private val addNewEvent = BroadcastChannel<News>(Channel.BUFFERED)
 
-    override fun getNews(request: Single<NewsRequest>?): Single<NewsResponse> {
-        return request?.map {
-            val news = NewsStore.getByTopic(it.topic)
-            NewsResponse.newBuilder().addAllNews(news).build()
-        } ?: Single.just(NewsResponse.newBuilder().build())
+    override suspend fun getNews(request: NewsRequest): NewsResponse {
+        val news = NewsStore.getByTopic(request.topic)
+        return NewsResponse.newBuilder().addAllNews(news).build()
     }
 
-    override fun subscribe(request: Single<SubscribeRequest>?): Flowable<News> {
-        return request?.toFlowable()?.flatMap { req->
-            addNewEvent.toFlowable(BackpressureStrategy.BUFFER).share().filter { it.topic == req.topic }
-
-        } ?: Flowable.empty<News>()
-
+    @FlowPreview
+    override fun subscribe(request: SubscribeRequest): Flow<News> {
+        return addNewEvent.asFlow().filter { it.topic == request.topic }
     }
 
-    override fun postNews(request: Single<News>?): Single<News> {
-        return request?.map {
-            NewsStore.saveNews(it)?.let {saved->
-                addNewEvent.onNext(saved)
-                saved
-            } ?: throw IllegalStateException("Save news failed")
-
-        } ?: Single.error<News>(NullPointerException())
+    override suspend fun postNews(request: News): News {
+        return NewsStore.saveNews(request)?.let {saved->
+            //addNewEvent.onNext(saved)
+            addNewEvent.send(saved)
+            saved
+        } ?: throw IllegalStateException("Save news failed")
     }
 }
